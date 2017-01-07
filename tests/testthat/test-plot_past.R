11c1bab2b0308d8ac3f4a2a11a8245182e341fa5
Colin Gillespie <csgillespie@gmail.com>
2017-01-07 22:55:34 +0000

    Test that blas_optimize runs

--- tests/testthat/test-plot_past.R
+++ tests/testthat/test-plot_past.R
@@ -2,7 +2,7 @@ test_that("Test plot_past", {
   skip_on_cran()
   expect_true(is.data.frame(plot_past()))
   expect_true(is.data.frame(plot_past(test=c("prog", "matrix_fun"))))
-  expect_true(is.data.frame(plot_past(byte_optimize = FALSE)))
+  expect_true(is.data.frame(plot_past(byte_optimize = FALSE, blas_optimize = TRUE)))
   expect_true(is.data.frame(plot_past(byte_optimize = TRUE)))
   expect_error(plot_past("testing"))
 }
